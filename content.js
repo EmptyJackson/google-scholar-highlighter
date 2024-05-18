392cf8331f1f69ef714899b366834db7f49e42df
Matthew Jackson <matthew@hmjackson.com>
2024-05-18 21:16:45 +0100

    Catch preferred names, multiple surnames, special characters

--- content.js
+++ content.js
@@ -2,13 +2,38 @@
 function applyStylesToPublications() {
     const publications = document.querySelectorAll('[class="gsc_a_tr"]');
     var pageAuthor = document.querySelector('#gsc_prf_in').textContent.trim().split(" ");
+    // Some people add brackets for preferred names, remove these
+    pageAuthor = pageAuthor.filter((name) => !name.includes('('));
+    // Get forename initials and surname
+    pageAuthorInitials = pageAuthor.slice(0, -1).map(
+        (name) => (name.includes('-'))
+            // Split names with dashes
+            ? name.split('-')[0].charAt(0) + name.split('-')[1].charAt(0)
+            : name.charAt(0)
+    ).join('');
     pageAuthorSurname = pageAuthor[pageAuthor.length - 1].replace(/[^\w\s]/gi, '');
     publications.forEach(function(pub) {
-        var authorsList = pub.querySelector('.gs_gray').textContent;
-        const coauthor = (name) => name.substring(0, name.length - 1).replace(/[^\w\s]/gi, '').concat(name[-1]).includes(pageAuthorSurname.concat("*"));
-        if (authorsList.split(",")[0].replace(/[^\w\s]/gi, '').includes(pageAuthorSurname) || authorsList.split(",").some(coauthor)) {
-            // pub.style.backgroundColor = '#F5F38D !important'
-            // pub.style.setProperty('background-color', '#FFFCAE', 'important');
+        var isFirstAuthor = false;
+        var authorsList = pub.querySelector('.gs_gray').textContent.split(",");
+        var firstAuthors = authorsList.filter((name) => name.includes('*'))
+        firstAuthors = firstAuthors.map((name) => name.replace('*', ''))
+        firstAuthors.push(authorsList[0].replace('*', ''))
+        // firstAuthors is a list of authors with asterisks removed
+        function surnameMatches(name) {
+            return name.replace(/[^\w\s]/gi, '').includes(pageAuthorSurname)
+        }
+        if (authorsList.map(surnameMatches).filter(Boolean).length > 1) {
+            // Repeated surname, check first authors contains full name
+            isFirstAuthor = firstAuthors.some((name) =>
+                surnameMatches(name)
+                && name.substring(0, pageAuthorInitials.length) == pageAuthorInitials
+            )
+        } else {
+            // At most one raw surname match, check first authors contains surname
+            isFirstAuthor = firstAuthors.some(surnameMatches);
+        }
+        // Update styling if first author
+        if (isFirstAuthor) {
             var nodes = pub.childNodes;
             for(var i=0; i<nodes.length; i++) {
                 if (nodes[i].nodeName.toLowerCase() == 'td') {
@@ -31,7 +56,7 @@ const observer = new MutationObserver(mutations => {
 });
 
 // Configuration for the observer: monitor additions of child elements
-const config = { childList: true, subtree: true, attributeFilter: ['class', 'role'], };
+const config = { childList: true, subtree: true, attributeFilter: ['class'], };
 
 // Start observing the document body for mutations
 observer.observe(document.body, config);
